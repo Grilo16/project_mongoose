f4692217a3d84697f1a22c491bb198a60a46e417
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-09-26 15:09:43 +0100

    omgitneverends

--- client/src/repositories/comment_repository.js
+++ client/src/repositories/comment_repository.js
@@ -6,26 +6,29 @@ const commentRepo = {
     },
     
     async addCommentToDb(commentObject){
-        fetch("http://localhost:9000/comment/add", {
+        const fetchUrl = await fetch("http://localhost:9000/comment/add", {
             method:"POST",
             body:JSON.stringify(commentObject),
             headers:{"Content-Type":"application/json"}
         })
+        return await fetchUrl.json()
+
     },
 
     async deleteCommentByID(id){
-        fetch(`http://localhost:9000/comment/delete/${id}`, {
+        const fetchUrl = await fetch(`http://localhost:9000/comment/delete/${id}`, {
             method:"DELETE"
         })
-
+        return await fetchUrl.json()
     },
 
     async editCommentById(id, patchObject){
-        fetch(`http://localhost:9000/comment/edit/${id}`, {
+        const fetchUrl = await fetch(`http://localhost:9000/comment/edit/${id}`, {
             method:"PATCH",
             body:JSON.stringify(patchObject),
             headers:{"Content-Type":"application/json"}
         })
+        return await fetchUrl.json()
     }
 
 
